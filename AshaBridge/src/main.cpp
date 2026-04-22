cdcd77e237c0acc62e88d695d82600bb9acfca52
pius-code <obliepius13@gmail.com>
2026-04-22 12:51:56 +0000

    minor fix

--- AshaBridge/src/main.cpp
+++ AshaBridge/src/main.cpp
@@ -1,29 +1,22 @@
-#include <Arduino.h>
 #include <ASHA.h>
-
-ASHA_WIFI ashaWifi;
-ASHA_Devices ashaDevices;
+#include <Arduino.h>
+ASHA asha;
 ASHA_Actuators ashaActuators;
 
-
 void afterConnection() {
-  Serial.println("ASHA: Internet Connected ");
+    Serial.println("ASHA: Internet Connected ");
 }
 
 const char* ssid = "Piusxel";
 const char* password = "voed5056";
 
-
-
-
 void setup() {
-ashaWifi.onConnect(afterConnection);
-ashaWifi.begin(ssid,password);
-ashaDevices.addDevice(ashaActuators.LED("test LED","digital"),12);
+    asha.asha_wifi.onConnect(afterConnection);
+    asha.asha_wifi.begin(ssid, password);
+    asha.asha_devices.addDevice(ashaActuators.LED("test LED", "digital"), 12);
+    asha.init("318f8vnie84su4jso3jfjw7");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 }
-
-
